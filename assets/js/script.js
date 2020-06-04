e0f2b8cc4bde92cfc123519995328ca6c550432f
Chris Hadley <hadleyc15@yahoo.com>
2020-06-04 16:38:19 -0600

    overall adjustments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,3 @@
 
-api.openweathermap.org/data/2.5/forecast?q={city name}&appid={e8797fa70655bf6d685414b1ebccf5e5};
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=e8797fa70655bf6d685414b1ebccf5e5";
+
