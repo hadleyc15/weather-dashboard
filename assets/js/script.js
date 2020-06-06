8307d5cf96bb628e373f544dd1542f279b670af9
Chris Hadley <hadleyc15@yahoo.com>
2020-06-06 10:46:08 -0600

    added conversion for temp to fahrenheit

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,27 @@ $(document).ready(function () {
                 searchArr.push(response.name)
                 localStorage.setItem("cities", JSON.stringify(searchArr));
             }
+            //transfer content to HTML
+            var currentCity = $(".jumbotron").addClass("city-weather").text(cityName + " Weather Details  ");
+            var currentDate = moment().format("  MM-DD-YYYY");
+            var windData = $("<p>").text("Wind Speed: " + response.wind.speed).addClass("lead");
+            var humidityData = $("<p>").text("Humidity: " + response.main.humidity + "%").addClass("lead");
+            var iconcode = response.weather[0].icon;
+            var iconurl = "https://openweathermap.org/img/w/" + iconcode + ".png";
+            var weatherImg = $("<img>").attr("src", iconurl);
+            var date = $("<p>").text(moment.unix().format("MMM Do YY")).addClass("lead");
+            $("#five-day").empty();
+            // Convert the temp from kelvin to fahrenheit
+            var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+            var roundedTemp = Math.floor(tempF);
+
+            //temp elements added to html
+            var tempDataF = $("<p>").text("Temp (F): " + roundedTemp + "°").addClass("lead");
+
+            //append the elements together
+            currentCity.append(weatherImg, currentDate, windData, humidityData, tempDataF);
+            $("container").append(currentCity);
+
         
 
 })
