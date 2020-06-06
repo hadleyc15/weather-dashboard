1d822879b7e44afc99bc2e63371895f9870dffda
Chris Hadley <hadleyc15@yahoo.com>
2020-06-06 13:49:53 -0600

    updates to js file still has a few bugs

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,65 @@ $(document).ready(function () {
                 renderSearchList();
             })
 
-           
+            //beggining of five day forcast portion
+            var fiveDayForcast = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + APIKey;
+
+            for (var i = 1; i < 6; i++) {
+                $.ajax({
+                    url: fiveDayForcast,
+                    type: "GET"
+                }).then(function (responseFiveDayForcast) {
+                    var cardbodyElem = $("<div>").addClass("card-body");
+
+                    var fiveDayCard = $("<div>").addClass(".cardbody");
+                    var fiveDate = $("<h5>").text(moment.unix(responseFiveDayForcast.daily[i].dt).format("MM/DD/YYYY"));
+                    fiveDayCard.addClass("headline");
+
+                    var fiveDayTemp = $("<p>").text("Temp: " + responseFiveDayForcast.daily[i].temp.max + "°");
+                    fiveDayTemp.attr("id", "#fiveDayTemp[i]");
+
+                    var fiveHumidity = $("<p>").attr("id", "humDay").text("Humidity: " + JSON.stringify(responseFiveDayForcast.daily[i].humidity) + "%");
+                    fiveHumidity.attr("id", "#fiveHumidity[i]");
+
+                    var iconCode = responseFiveDayForcast.daily[i].weather[0].icon;
+                    var iconURL = "https://openweathermap.org/img/w/" + iconCode + ".png";
+                    var weatherImgDay = $("<img>").attr("src", iconURL);
+                    $("#testImage").attr("src", iconURL);
+
+                    cardbodyElem.append(fiveDate);
+                    cardbodyElem.append(weatherImgDay);
+                    cardbodyElem.append(fiveDayTemp);
+                    cardbodyElem.append(fiveHumidity);
+                    fiveDayCard.append(cardbodyElem);
+                    $("#five-day").append(fiveDayCard);
+                    $("#fiveDayTemp[i]").empty();
+                    $(".jumbotron").append(cardbodyElem);
+                })
+            }
+            $("#search").val("");
+
+        })
+
+    }
+    //turns previously searched cities in list in to buttons for easy searching
+    $(document).on("click", ".city-btn", function () {
+        JSON.parse(localStorage.getItem("cities"));
+        var cityName = $(this).text();
+        findCity(cityName);
+    });
+
+    function renderSearchList() {
+        var searchList = JSON.parse(localStorage.getItem("cities"));
+        $("#search-list").empty();
+        if (searchList) {
+            for (i = 0; i < searchList.length; i++) {
+                var listBtn = $("<button>").addClass("btn btn-secondary city-btn").attr('id', 'cityname_' + (i + 1)).text(searchList[i]);
+                var listElem = $("<li>").attr('class', 'list-group-item');
+                listElem.append(listBtn);
+                $("#search-list").append(listElem);
+            }
+
+        }
 
     }
 
